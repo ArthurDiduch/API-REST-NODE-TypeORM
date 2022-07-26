e2342ac346cfc856b5f9d12abcbb1dac8a946ac2
ArthurDiduch <arthurdiduch@gmail.com>
2022-07-26 13:27:04 -0300

    alterações

--- DIO/api-dio-node/src/controllers/CreateUserController.ts
+++ DIO/api-dio-node/src/controllers/CreateUserController.ts
@@ -1,11 +1,21 @@
-import { request, Request, Response} from 'express';
+import {Request, Response} from 'express';
+import { CreateUserService } from '../services/CreateUserService';
 
 class CreateUserController{
     handle(Request:Request, response:Response) {
+
+        const createUserService = new CreateUserService();
         
         const nome = Request.body.nome;
+        const email = Request.body.email;
 
-        return response.json({mensagem:`Usuário ${nome} criado`});
+        if(nome.length === 0 || email.length === 0){
+            return response.status(400).json({mensagem: 'Preencha todos os campos'});
+        }
+
+        const user = createUserService.execute({nome, email});
+
+        return response.status(201).json({user});
     }
 };
 
---
+++ DIO/api-dio-node/src/database/index.ts
@@ -0,0 +1,5 @@
+import { Connection, createConnection, getConnection } from 'typeorm';
+
+export default async(): Promise<Connection> => {
+    const defaultOption 
+}
