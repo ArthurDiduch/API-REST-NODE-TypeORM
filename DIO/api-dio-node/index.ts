91d1f667a4908fa470e002dba82ce5c9e2cfac77
ArthurDiduch <arthurdiduch@gmail.com>
2022-07-26 10:01:32 -0300

    alterações

---
+++ DIO/api-dio-node/index.ts
@@ -0,0 +1,11 @@
+import express, {Request, Response} from 'express';
+
+const server = express();
+
+server.get('/', (Request:Request, response:Response) => {
+    return response.json({mensagem: 'Bem vindo a nossa DIO API'});
+});
+
+server.listen(5000, () =>{
+    console.log('Servidor on na porta 5000 http://localhost:5000/');
+});
