099ff0f44ee88567c7bdc9be601a845390744d16
ArthurDiduch <arthurdiduch@gmail.com>
2022-07-27 12:47:27 -0300

    alterações

--- DIO/api-dio-node/src/controllers/CreateUserController.ts
+++ DIO/api-dio-node/src/controllers/CreateUserController.ts
@@ -1,4 +1,5 @@
 import {Request, Response} from 'express';
+import { v4 as uuid } from 'uuid';
 import { CreateUserService } from '../services/CreateUserService';
 
 class CreateUserController{
@@ -8,12 +9,13 @@ class CreateUserController{
         
         const nome = Request.body.nome;
         const email = Request.body.email;
+        const id = uuid();
 
         if(nome.length === 0 || email.length === 0){
             return response.status(400).json({mensagem: 'Preencha todos os campos'});
         }
 
-        const user = createUserService.execute({nome, email});
+        const user = createUserService.execute({id, nome, email});
 
         return response.status(201).json({user});
     }
--- DIO/api-dio-node/src/entities/Usuario.ts
+++ DIO/api-dio-node/src/entities/Usuario.ts
@@ -2,7 +2,14 @@ import { Entity, Column, PrimaryColumn } from 'typeorm';
 
 @Entity('usuarios')
 class Usuario{
-    
+    @PrimaryColumn()
+    id: string;
+
+    @Column()
+    nome: string
+
+    @Column()
+    email: string
 }
 
 export { Usuario }
