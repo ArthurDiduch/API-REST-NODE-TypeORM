091057c571371d8ed8648a704a45e3caf5e96f5a
ArthurDiduch <arthurdiduch@gmail.com>
2022-07-27 10:38:24 -0300

    alterações

--- DIO/api-dio-node/src/database/index.ts
+++ DIO/api-dio-node/src/database/index.ts
@@ -1,5 +1,8 @@
 import { Connection, createConnection, getConnection } from 'typeorm';
 
 export default async(): Promise<Connection> => {
-    const defaultOption 
+    const defaultOption = await getConnection();
+    return createConnection(
+        Object.assign(defaultOption)
+    )
 }
---
+++ DIO/api-dio-node/src/entities/Usuario.ts
@@ -0,0 +1,8 @@
+import { Entity, Column, PrimaryColumn } from 'typeorm';
+
+@Entity('usuarios')
+class Usuario{
+    
+}
+
+export { Usuario }
